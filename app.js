d390166fbc0994cb545ea1ff48021510b1ae9829
halawaoji97 <ozidhalawa97@gmail.com>
2022-01-03 18:28:55 +0700

    redeploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 const methodOverride = require('method-override');
 const session = require('express-session');
@@ -53,6 +54,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   '/sb-admin-2',
